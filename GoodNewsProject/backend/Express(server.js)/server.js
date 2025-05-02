f4f49edf349fda057f909ea9eae962ac6f363516
thedoctor537 <gili.mayer100@gmail.com>
2025-05-02 14:30:32 +0300

    update

--- GoodNewsProject/backend/Express(server.js)/server.js
+++ GoodNewsProject/backend/Express(server.js)/server.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 app.use(express.json());
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/HTML', 'login.html'))
+});
 
 app.post("/login", (req, res) => {
+    console.log(req.body);
     const { email, password } = req.body;
 
     if (!email || !password){
@@ -10,6 +19,10 @@ app.post("/login", (req, res) => {
     }
 
     res.send("User logged in!");
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/HTML', 'index.html'))
 });
 
 app.listen(3000, () => console.log("Server running on port 3000"));

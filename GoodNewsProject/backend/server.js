88aba9287f11448498bdb8874cf3b12b81f53fbc
thedoctor537 <gili.mayer100@gmail.com>
2025-05-23 18:23:36 +0300

    update

--- GoodNewsProject/backend/server.js
+++ GoodNewsProject/backend/server.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.json());
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
-const db = require("../database/server/db");
+const { db, db2 } = require("../database/server/db");
 
 app.use(express.static(path.join(__dirname, "../public/style")))
 console.log(__dirname);
@@ -18,10 +18,17 @@ app.post("/login", (req, res) => {
     const { email, password } = req.body;
 
     if (!email || !password){
-        return res.status(400).json({ error: "All fields are required" });
+        return res.status(400).json({ error: "!יש למלא את כל השדות" });
     }
 
     res.send("נכנסת למשתמש שלך בהצלחה");
+
+    db.get(`SELECT * FROM users WHERE email = ? AND password = ?`, [email, password], (err, user) => {
+    if (err) return res.status(500).send("שגיאה בשרת");
+    if (!user) return res.status(401).send("אימייל או סיסמה לא נכונים");
+
+    res.send(`שלום ${email}, התחברת בהצלחה!`);
+  });
 });
 
 app.get("/signup", (req, res) => {
@@ -29,19 +36,34 @@ app.get("/signup", (req, res) => {
 });
 
 app.post("/signup", (req, res) => {
-    const { username, password } = req.body;
+  const { email, password, password2 } = req.body;
+  console.log(req.body);
 
-    db.run(`INSERT INTO users (username, password) VALUES (?,?)`,
-        [username, password]
-    );
+  if (password !== password2) {
+    return res.send("סיסמה לא תקינה");
+  }
 
-    if (!email || !password){
-        return res.status(400).json({ error: "All fields are required" });
+  db.get(`SELECT * FROM users WHERE email = ?`, [email], (err, row) => {
+    if (err) {
+      return res.send("שגיאה בבדיקה");
     }
 
-    res.send("נרשמת בהצלחה");
-});
+    if (row) {
+      return res.send("שם המשתמש כבר קיים");
+    }
 
+    db.run(
+      `INSERT INTO users (email, password) VALUES (?, ?)`,
+      [email, password],
+      function (err) {
+        if (err) {
+          return res.send("שגיאה בהכנסת המשתמש");
+        }
+        return res.send("נרשמת בהצלחה!");
+      }
+    );
+  });
+});
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/HTML', 'index.html'))
 });
@@ -53,5 +75,26 @@ app.get("/about", (req, res) => {
 app.get("/makeContact", (req, res) => {
     res.sendFile(path.join(__dirname, '../public/HTML', 'makeContact.html'))
 });
+
+app.post("/makeContact", (req, res) => {
+  console.log(req.body);
+  const { fullName, phone, email, message } = req.body;
+
+  if (!fullName || !phone || !email || !message) {
+    return res.status(400).json({ error: "!יש למלא את כל השדות" });
+  }
+
+  db2.run(
+    `INSERT INTO requests (fullName, phone, email, message) VALUES (?, ?, ?, ?)`,
+    [fullName, phone, email, message],
+    function (err) {
+      if (err) {
+        return res.status(500).send("אירעה שגיאה בעת שמירת הבקשה");
+      }
+      res.send("בקשתך נשלחה בהצלחה, נחזור אליך בהקדם האפשרי.");
+    }
+  );
+});
+
 
 app.listen(3000, () => console.log("Server running on port 3000"));
--- GoodNewsProject/database/server/db.js
+++ GoodNewsProject/database/server/db.js
@@ -2,13 +2,24 @@ const sqlite3 = require("sqlite3");
 const path = require("path");
 const Module = require("module");
 const db = new sqlite3.Database(path.join(__dirname, "db", "users.db"));
+const db2 = new sqlite3.Database(path.join(__dirname, "db", "requests.db"))
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS users(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
     password TEXT NOT NULL
     )`);
 });
 
-module.exports = db;
+db2.serialize(() => {
+  db2.run(`CREATE TABLE IF NOT EXISTS requests(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    fullName TEXT NOT NULL,
+    phone TEXT NOT NULL,
+    email TEXT NOT NULL,
+    message TEXT NOT NULL
+  )`);
+});
+
+module.exports = { db, db2 };
